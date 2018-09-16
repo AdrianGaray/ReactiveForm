5068cbcf987f563628ab44aa8f52eb06b69bc5ca
adrian <adriangaray396@gmail.com>
2018-09-16 19:35:57 -0300

    Ignore files in commits already made and pushed(6)

---
+++ reactiveFormWeb/ClientApp/dist_____/testAG.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace reactiveFormWeb.ClientApp.dist_____
+{
+  public class testAG
+  {
+        public int MyProperty { get; set; }
+    }
+}
